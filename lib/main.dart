396777b58315f0cfe30947a0fbad283700309a32
magdaalesh <magodalesh@gmail.com>
2025-03-21 17:03:14 +0300

    page1

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,16 @@ class NotesApp extends StatelessWidget {
     return const Placeholder();
   }
 }
+class page1 extends StatefulWidget {
+  const page1({super.key});
+
+  @override
+  State<page1> createState() => _page1State();
+}
+
+class _page1State extends State<page1> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
