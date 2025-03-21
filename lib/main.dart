6a9e5ce410c08ffe50a15c1c913d07b60390c0cc
magdaalesh <magodalesh@gmail.com>
2025-03-21 17:50:28 +0300

    create notes view

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:to_do/views/notes_view.dart';
 
 void main() {
   runApp(const NotesApp());
 }
+
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-class page1 extends StatefulWidget {
-  const page1({super.key});
-
-  @override
-  State<page1> createState() => _page1State();
-}
-
-class _page1State extends State<page1> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: NotesView(),
+    );
   }
 }
